b248b3585f97bc07d2ccb77a1d99da88e661aa84
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-02-07 00:01:57 +0600

    json stingify

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
+//stringify is basically make every value and property from an object make it in string like in the double quote("");
+
 const user = {id :1, name: 'gorib amir', job: 'actor'};
 const stingified = JSON.stringify(user);
-
+// const parse = JSON.parse(user);
 console.log(user);
 console.log(stingified);
 
