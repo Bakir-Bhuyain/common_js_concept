691222065a74427d0128d36268ae6d2f1fe2150a
Bakir-Bhuyain <hemalbakir@gmail.com>
2026-02-07 00:14:53 +0600

    explored the json stringfy and parse with example

--- api.js
+++ api.js
@@ -3,6 +3,27 @@
 const user = {id :1, name: 'gorib amir', job: 'actor'};
 const stingified = JSON.stringify(user);
 // const parse = JSON.parse(user);
-console.log(user);
-console.log(stingified);
+// console.log(user);
+// console.log(stingified);
 
+
+
+// JSON.parse start from here;
+
+const shop ={
+      owner: 'alia',
+      address : {
+            street : 'ranbir',
+            city : 'kocu khet vhuter goli',
+            country : 'BD',
+      },
+      products : ['laptop', 'mic', 'monitor', 'keyboard'],
+      revenue : 4500000,
+      isOpen : true,
+      isNew : false,
+};
+console.log(shop);
+const shopJson = JSON.stringify(shop);
+const shopObj = JSON.parse(shopJson);
+console.log(shopJson);
+console.log(shopObj);
